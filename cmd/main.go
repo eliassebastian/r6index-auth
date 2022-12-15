7a795712b1f5fea076cf3e308675f6855af800cd
Elias Sebastian <eeschmoelz@gmail.com>
2022-12-15 18:43:06 +0000

    feat: initial project files

--- cmd/main.go
+++ cmd/main.go
@@ -1,1 +1,136 @@
 package main
+
+import (
+	"context"
+	"errors"
+	"flag"
+	"log"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
+
+	"github.com/eliassebastian/r6index-auth/internal/rabbitmq"
+	"github.com/eliassebastian/r6index-auth/internal/ubisoft"
+	"github.com/go-co-op/gocron"
+	"github.com/joho/godotenv"
+)
+
+func main() {
+	log.Println("::::::::: PRE R6 INDEX AUTH STARTING")
+	var env string
+
+	flag.StringVar(&env, "env", "", "Environment Variables filename")
+	flag.Parse()
+
+	err := godotenv.Load(env)
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+
+	errC, err := run()
+	if err != nil {
+		log.Fatalf("Couldn't run: %s", err)
+	}
+
+	if err := <-errC; err != nil {
+		log.Fatalf("Error while running: %s", err)
+	}
+
+	log.Println(os.Getenv("UBISOFT_URL"))
+}
+
+type serverConfig struct {
+	rabbitmq  *rabbitmq.RabbitMQConfig
+	scheduler *gocron.Scheduler
+	ubisoft   *ubisoft.UbisoftConfig
+	doneC     chan struct{}
+}
+
+func run() (<-chan error, error) {
+	//fetch initial ubisoft auth details
+	ubi := ubisoft.NewUbisoftConnection()
+
+	//start rabbitmq connection as producer
+	rq, err := rabbitmq.NewConnection()
+	if err != nil {
+		return nil, err
+	}
+
+	//start cron scheduler
+	s := gocron.NewScheduler(time.UTC)
+
+	srv := &serverConfig{
+		rabbitmq:  rq,
+		ubisoft:   ubi,
+		scheduler: s,
+	}
+
+	errC := make(chan error, 1)
+	ctx, stop := signal.NotifyContext(context.Background(), os.Interrupt, syscall.SIGTERM, syscall.SIGQUIT)
+
+	go func() {
+		<-ctx.Done()
+		log.Println("::::::::: Shutdown signal received")
+		ctxTimeout, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+
+		defer func() {
+			err := rq.Close()
+			if err != nil {
+				log.Println("Failed to close Kafka Connection")
+			}
+
+			ubi.Stop()
+			s.Stop()
+
+			stop()
+			cancel()
+			close(errC)
+		}()
+
+		if err := srv.shutdown(ctxTimeout); err != nil {
+			errC <- err
+		}
+
+		log.Println("::::::::: Shutdown Finished")
+	}()
+
+	go func() {
+		if err := srv.listenAndServe(); err != nil {
+			errC <- err
+		}
+	}()
+
+	return errC, nil
+}
+
+func (s *serverConfig) listenAndServe() error {
+	log.Println(":::::: ListenAndServer")
+	//TODO initiate cron job every 2hr45min
+	//s.scheduler.Every("2h45m").Do()
+	job, err := s.scheduler.Every("10m").Do(func(con *rabbitmq.RabbitMQConfig) {
+		err := s.ubisoft.Connect(context.Background(), con)
+		if err != nil {
+			log.Println("Job Error", err)
+		}
+		log.Println("SUCCESS UBI")
+	}, s.rabbitmq)
+
+	log.Println(job, err)
+	s.scheduler.StartBlocking()
+
+	log.Println("Scheduler Stopped")
+	return nil
+}
+
+func (s *serverConfig) shutdown(ctx context.Context) error {
+	log.Println(":::::: Shutting Down Server")
+	for {
+		select {
+		case <-ctx.Done():
+			return errors.New("Context.Done Error")
+		case <-s.doneC:
+			return nil
+		}
+	}
+}
--- go.mod
+++ go.mod
@@ -1,3 +1,14 @@
 module github.com/eliassebastian/r6index-auth
 
 go 1.19
+
+require (
+	github.com/go-co-op/gocron v1.18.0 // indirect
+	github.com/joho/godotenv v1.4.0 // indirect
+	github.com/rabbitmq/amqp091-go v1.5.0 // indirect
+	github.com/robfig/cron/v3 v3.0.1 // indirect
+	go.uber.org/atomic v1.7.0 // indirect
+	go.uber.org/multierr v1.6.0 // indirect
+	go.uber.org/zap v1.24.0 // indirect
+	golang.org/x/sync v0.0.0-20210220032951-036812b2e83c // indirect
+)
